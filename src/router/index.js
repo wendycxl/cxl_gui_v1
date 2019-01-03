de70b23a9bd11716571dcd492dc434e779003f91
chenxiaolan <xiaolan.chen@37duc.com>
2019-01-03 14:37:46 +0800

    修改

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,21 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import HelloWorld from 'components/HelloWorld'
 
+const Login = (resolve) => {
+  import('components/login').then((module) => {
+    resolve(module)
+  })
+}
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     {
       path: '/',
       name: 'HelloWorld',
